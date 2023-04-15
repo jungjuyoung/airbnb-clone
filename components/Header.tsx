32a71d7d177fc52368b49e5c95a808968d132ea1
jungjuyoung <nwwn1004@naver.com>
2023-04-15 21:16:41 +0900

    building search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+
 export const Header = () => {
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
@@ -11,7 +13,16 @@ export const Header = () => {
           objectPosition='left'
         />
       </div>
+
       {/* Middle */}
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input
+          type='text'
+          className='flex-grow pl-5 bg-transparent outline-none'
+          placeholder='Start your search'
+        />
+        <MagnifyingGlassIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
+      </div>
       {/* Right */}
     </header>
   );
