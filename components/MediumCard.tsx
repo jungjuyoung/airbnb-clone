9a3804c11bb5df720eac45f574894aa6164569f4
jungjuyoung <nwwn1004@naver.com>
2023-04-18 23:38:01 +0900

    created Meidum card

---
+++ components/MediumCard.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+interface cardsFetchInterface {
+  img: string;
+  title: string;
+}
+export const MediumCard = ({ img, title }: cardsFetchInterface) => {
+  return (
+    <div className='cursor-pointer hover:scale-105 transform transition duration-300 ease-out'>
+      <div className='relative h-80 w-80'>
+        <Image src={img} layout='fill' alt={title} />
+      </div>
+      <h3 className='text-2xl mt-3'>{title}</h3>
+    </div>
+  );
+};
