e995e2fc3e8a1b696b391c87d559602e6fd5d3de
jungjuyoung <nwwn1004@naver.com>
2023-04-18 23:35:52 +0900

    created smallcard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import { Banner } from '@/components/Banner';
 import { Header } from '@/components/Header';
+import { MediumCard } from '@/components/MediumCard';
 import { isTemplateExpression } from 'typescript';
+import { SmallCard } from '../components/SmallCard';
 
 interface exploreInterface {
   img: string;
   location: string;
   distance: string;
 }
+interface cardsFetchInterface {
+  img: string;
+  title: string;
+}
+
 const exploreData: Array<exploreInterface> = [
   {
     img: 'https://links.papareact.com/5j2',
@@ -49,11 +56,19 @@ const exploreData: Array<exploreInterface> = [
     distance: '2-hour drive',
   },
 ];
+const cardsData: Array<cardsFetchInterface> = [
+  { img: 'https://links.papareact.com/2io', title: 'Outdoor getaways' },
+  { img: 'https://links.papareact.com/q7j', title: 'Unique stays' },
+  { img: 'https://links.papareact.com/s03', title: 'Entire homes' },
+  { img: 'https://links.papareact.com/8ix', title: 'Pet allowed' },
+];
 
 export default function Home({
   exploreData,
+  cardsData,
 }: {
   exploreData: exploreInterface;
+  cardsData: cardsFetchInterface;
 }) {
   return (
     <div>
@@ -67,9 +82,26 @@ export default function Home({
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
           {/* Pull some data from a server - API endpoints */}
-          {exploreData?.map((item, index) => (
-            <h1 key={index}>{item.location}</h1>
-          ))}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(
+              ({ img, distance, location }: exploreInterface) => (
+                <SmallCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  distance={distance}
+                />
+              )
+            )}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll'>
+            {cardsData?.map(({ img, title }: cardsFetchInterface) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
@@ -81,7 +113,12 @@ export async function getStaticProps() {
   // 500 server error maybe cors error I think.... so I don't use this.
   // create an asyn function called getStaticProps and fetch data export it as an object as a return value,
   // you can receive it as a prop in the component function.
+
+  // const cardsData = await fetch('https://links.papareact.com/zp1').then(res =>
+  //   res.json()
+  // );
+
   return {
-    props: { exploreData },
+    props: { exploreData, cardsData },
   };
 }
